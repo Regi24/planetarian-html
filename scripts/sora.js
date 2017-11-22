9cdd08584fec0ecb90575147438e2a9833ef5898
Regi24 <Regi24@users.noreply.github.com>
2017-11-22 14:53:45 +1100

    Asthetics

--- scripts/sora.js
+++ scripts/sora.js
@@ -5,7 +5,7 @@ var music = new Audio('');
 // Load external JSON file into scenes array
 function loadJSON() {
 	//$.getJSON("https://raw.githubusercontent.com/Regi24/planetarian-html/master/assets/story/scenes.json?token=AHEVrezP4j5AuUVEjfowGdiTz8x01e6Kks5aHUvkwA%3D%3D", function(data) {
-		$.getJSON("./assets/story/scenes.json", function(data) {
+	$.getJSON("./assets/story/scenes.json", function(data) {
 		scenes = data;
 		//console.log(scenes[0].title);
 	});
@@ -13,7 +13,7 @@ function loadJSON() {
 
 function nextScene() {
 	if(counter < scenes.length) {
-		document.getElementById("speech").innerHTML = scenes[counter].title;
+		document.getElementById("dialogue").innerHTML = scenes[counter].title;
 		playMusic(scenes[counter].music);
 		counter++;
 	} 
